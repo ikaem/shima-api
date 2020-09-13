974e5e2f0553ead2e0f5b847d9da007f26ea7692
Karlo Marinović <marinovic.karlo@gmail.com>
2020-09-13 10:23:05 +0200

    finished basic messaging between server and frontedn

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import http from "http";
-import socketio from "socket.io";
+// import socketio from "socket.io";
+
+
 
 import routes from "./router";
+import ioServer from "./socket-io/server";
 
 const app = express();
 
@@ -16,39 +19,48 @@ server.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
 
-// socket io
-const io = socketio(server);
+ioServer(server);
 
-let userName: string;
 
-io.on("connect", (socket) => {
-  console.log("We have a connection", socket.id);
 
-  // listening for events
-  socket.on("setUsername", (data: { name: string }) => {
-    userName = data.name;
 
-    socket.emit("adminMessage", { name: "admin", content: "Hi kaem, welcome to the chat"})
 
-    // test from here
-    socket.emit("adminMessage", { name: "kaem", content: "Hi kaem, welcome to the chat", room: "lobby"})
-    socket.emit("adminMessage", { name: "john", content: "Hi kaem, welcome to the chat", room: "john"})
-    socket.emit("adminMessage", { name: "mark", content: "Hi kaem, welcome to the chat", room: "mark"})
-    socket.emit("adminMessage", { name: "kaem", content: "Hi kaem, welcome to the chat", room: "bday"})
-    socket.emit("adminMessage", { name: "admin", content: "Hi kaem, welcome to the chat", room: "lobby"})
-    socket.emit("adminMessage", { name: "kaem", content: "Hi kaem, welcome to the chat", room: "tech"})
-    socket.emit("adminMessage", { name: "admin", content: "Hi kaem, welcome to the chat", room: "lobby"})
-    socket.emit("adminMessage", { name: "mark", content: "Hi kaem, welcome to the chat", room: "mark"})
-    socket.emit("adminMessage", { name: "karm", content: "Hi kaem, welcome to the chat", room: "lobby"})
-    socket.emit("adminMessage", { name: "karm", content: "Hi kaem, welcome to the chat", room: "lobby"})
-    socket.emit("adminMessage", { name: "karm", content: "Hi kaem, welcome to the chat", room: "lobby"})
+// // socket io
+// const io = socketio(server);
 
-  });
+// io.on("connect", (socket) => {
+//   console.log("We have a connection", socket.id);
 
-  
+//   // listening for events
+//   socket.on("join", (data: { name: string; room: string }) => {
+//     // here we check that the user is not in the room already
 
-  // disconnect event on socket
-  socket.on("disconnect", () => {
-    console.log("the socket has disconnected");
-  });
-});
+//     // then we send admin message saying welcome
+
+//     // we send it just to the socket here
+//     socket.emit("adminMessage", {
+//       name: "admin",
+//       content: `Hi ${data.name}, welcome to the chat`,
+//       room: data.room,
+//     });
+
+//     // we send to everyone here in this room, but the user
+//     socket.to(data.room).emit("adminMessage", {
+//       name: "admin",
+//       content: `${data.name} "has joined the room`,
+//       room: data.room,
+//     });
+
+//     // we send to everyone here in the chat
+//     socket.emit("adminMessage", {
+//       name: "admin",
+//       content: `You all have fun`,
+//       room: data.room,
+//     });
+//   });
+
+//   // disconnect event on socket
+//   socket.on("disconnect", () => {
+//     console.log("the socket has disconnected");
+//   });
+// });
