7f07a26d59b8059c046c93ce24e418f78406ed8a
Karlo Marinović <marinovic.karlo@gmail.com>
2020-09-15 13:40:31 +0200

    prior to deploy to heroku

--- src/server.ts
+++ src/server.ts
@@ -5,61 +5,19 @@ import http from "http";
 import routes from "./router";
 import ioServer from "./socket-io/server";
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 const server = http.createServer(app);
 
-server.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+server.listen(PORT, () => {
+  console.log("Server is listening on port", PORT);
 });
 
 ioServer(server);
-
-
-
-
-
-// // socket io
-// const io = socketio(server);
-
-// io.on("connect", (socket) => {
-//   console.log("We have a connection", socket.id);
-
-//   // listening for events
-//   socket.on("join", (data: { name: string; room: string }) => {
-//     // here we check that the user is not in the room already
-
-//     // then we send admin message saying welcome
-
-//     // we send it just to the socket here
-//     socket.emit("adminMessage", {
-//       name: "admin",
-//       content: `Hi ${data.name}, welcome to the chat`,
-//       room: data.room,
-//     });
-
-//     // we send to everyone here in this room, but the user
-//     socket.to(data.room).emit("adminMessage", {
-//       name: "admin",
-//       content: `${data.name} "has joined the room`,
-//       room: data.room,
-//     });
-
-//     // we send to everyone here in the chat
-//     socket.emit("adminMessage", {
-//       name: "admin",
-//       content: `You all have fun`,
-//       room: data.room,
-//     });
-//   });
-
-//   // disconnect event on socket
-//   socket.on("disconnect", () => {
-//     console.log("the socket has disconnected");
-//   });
-// });
